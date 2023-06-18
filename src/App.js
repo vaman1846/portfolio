b03c8b02a4cb2433d59492b624d510ecafe1316d
vaman1846 <115336951+vaman1846@users.noreply.github.com>
2023-06-18 14:42:53 +0530

    Update index.html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './app.css';
 import Project from './components/Project/Project';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
